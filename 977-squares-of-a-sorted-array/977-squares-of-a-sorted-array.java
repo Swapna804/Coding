8156793f81634c33551b11bef0b852c2fd4a83b8
Swapna <65804603+Swapna804@users.noreply.github.com>
2022-07-26 21:44:14 +0530

    Time: 2 ms (85.31%), Space: 56.2 MB (13.99%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -4,21 +4,19 @@ class Solution {
         int i=0;
         int j=nums.length-1;
         int idx=nums.length-1;
-        
         while(i<=j){
             int val1=nums[i]*nums[i];
-              int val2=nums[j]*nums[j];
+            int val2=nums[j]*nums[j];
+            
             if(val1>val2){
                 res[idx]=val1;
                 i++;
             }else{
                 res[idx]=val2;
-                j--; 
+                j--;
             }
             idx--;
-            
         }
         return res;
-        
     }
 }
