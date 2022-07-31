74809c126335bce9d4ab57ad8f60d96b1d02b720
Swapna <65804603+Swapna804@users.noreply.github.com>
2022-07-31 14:50:55 +0530

    Time: 4 ms (75.28%), Space: 42.2 MB (98.53%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,23 +1,14 @@
 class Solution {
     public String firstPalindrome(String[] words) {
-        for(String word:words){
-            if(isPalindrome(word)){
+        StringBuilder sb = null;
+
+        for(String word: words){
+            sb = new StringBuilder(word);
+
+			//Check if word is palindrome
+            if(sb.reverse().toString().equals(word))
                 return word;
-            }
         }
         return "";
-    }
-    public boolean isPalindrome(String word){
-        char[] array=word.toCharArray();
-   
-        int i=0,j=array.length-1;
-        while(i<j){
-            if(array[i]!=array[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
     }
 }
